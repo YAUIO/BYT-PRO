5d3eb7853c9a0c05ae21a8f6a41c4ac577d5d163
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-11-09 01:33:05 +0100

    example validation tests

---
+++ BYTPRO.Test/Data/Validation/StringValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using BYTPRO.Data.Validation;
+using BYTPRO.Data.Validation.Validators;
+
+namespace BYTPRO.Test.Data.Validation;
+
+public class StringValidatorTests
+{
+    [Fact]
+    public void TestIsNotNullOrEmptyReturnsTrueForNotNullOrEmpty()
+    {
+        const string str = "not-null";
+        str.IsNotNullOrEmpty();
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void TestIsNotNullOrEmptyThrowsForNull()
+    {
+        string? str = null;
+        Assert.Throws<ValidationException>(() => str.IsNotNullOrEmpty());
+    }
+    
+    [Fact]
+    public void TestIsNotNullOrEmptyThrowsForEmpty()
+    {
+        const string str = "";
+        Assert.Throws<ValidationException>(() => str.IsNotNullOrEmpty());
+    }
+}
