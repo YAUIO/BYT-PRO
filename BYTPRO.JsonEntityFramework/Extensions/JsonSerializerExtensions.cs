d425188bf107156376ba822985e3e30debb6fd1c
dredd <1misha.baltovsky@gmail.com>
2025-11-13 10:52:08 +0100

    Update `JsonSerializerOptions` to use relaxed JSON escaping encoder

--- BYTPRO.JsonEntityFramework/Extensions/JsonSerializerExtensions.cs
+++ BYTPRO.JsonEntityFramework/Extensions/JsonSerializerExtensions.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace BYTPRO.JsonEntityFramework.Extensions;
 
 public static class JsonSerializerExtensions
 {
-    internal static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
-    
+    internal static readonly JsonSerializerOptions Options = new()
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
     public static string ToJson(this IEnumerable collection)
     {
         return JsonSerializer.Serialize(collection, Options);
