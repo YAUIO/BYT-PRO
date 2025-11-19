7dbb4ce72ed805df0a52ee76af35cace986df0c8
YAUIO <nbyauio@gmail.com>
2025-11-19 09:02:59 +0100

    wrote tests for list

---
+++ BYTPRO.Test/Jef/Context/DeserializableReadOnlyListTests.cs
@@ -0,0 +1,48 @@
+using BYTPRO.JsonEntityFramework.Context;
+
+namespace BYTPRO.Test.Jef.Context;
+
+public class DeserializableReadOnlyListTests
+{
+    [Fact]
+    public void TestStores()
+    {
+        var list = new DeserializableReadOnlyList<string>();
+        
+        var str = "hello";
+        
+        list.Add(str);
+
+        Assert.Contains(str, list);
+    }
+    
+    [Fact]
+    public void TestStoresAfterReadOnly()
+    {
+        var list = new DeserializableReadOnlyList<string>();
+        
+        var str = "hello";
+        
+        list.Add(str);
+        
+        list.MakeReadOnly();
+
+        Assert.Contains(str, list);
+    }
+    
+    [Fact]
+    public void TestReadOnly()
+    {
+        var list = new DeserializableReadOnlyList<string>();
+        
+        var str = "hello";
+        
+        list.Add(str);
+        
+        list.MakeReadOnly();
+
+        Assert.Contains(str, list);
+        Assert.Throws<NotSupportedException>(() => list.Add(str));
+        Assert.True(list.IsReadOnly);
+    }
+}
