c2289f5dbb0f1f4efcd76b2e155553d8c71797fc
Maksym-Kalistratov <4702323@gmail.com>
2025-12-02 00:55:40 +0100

    Created aggregation association between `Branch` and `Product` models; added `BranchProductStock` class and updated related constructors, methods, and delete logic accordingly.

---
+++ BYTPRO.Data/Models/Locations/Branches/BranchProductStock.cs
@@ -0,0 +1,48 @@
+using System.Text.Json.Serialization;
+using BYTPRO.Data.Validation.Validators;
+
+namespace BYTPRO.Data.Models.Locations.Branches;
+
+using Sales;
+
+public class BranchProductStock
+{
+    // ----------< Attributes >----------
+    private int _quantity;
+
+    // ----------< Associations >----------
+    public Branch Branch { get; }
+    public Product Product { get; }
+
+    // ----------< Properties >----------
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            value.IsNonNegative(nameof(Quantity));
+            _quantity = value;
+        }
+    }
+
+    // ----------< Constructor >----------
+    public BranchProductStock(Branch branch, Product product, int quantity)
+    {
+        branch.IsNotNull(nameof(branch));
+        product.IsNotNull(nameof(product));
+        quantity.IsNonNegative(nameof(quantity));
+
+        Branch = branch;
+        Product = product;
+        Quantity = quantity;
+
+        Branch.AddStock(this);
+        Product.AddStock(this);
+    }
+
+    public void Dissolve()
+    {
+        Branch.RemoveStock(this);
+        Product.RemoveStock(this);
+    }
+}
