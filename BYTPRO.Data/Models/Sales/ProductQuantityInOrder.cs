839eef607c0682797aa405e35ee6e37b141414a3
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-12-03 15:16:48 +0100

    added validation

--- BYTPRO.Data/Models/Sales/ProductQuantityInOrder.cs
+++ BYTPRO.Data/Models/Sales/ProductQuantityInOrder.cs
@@ -6,10 +6,38 @@ namespace BYTPRO.Data.Models.Sales;
 
 public record ProductQuantityInOrder
 {
+    private readonly Product _product;
+    private readonly Order _order;
+    private readonly int _quantity;
+
     // ----------< Properties >----------
-    public Product Product { get; }
-    public Order Order { get; }
-    public int Quantity { get; }
+    public Product Product
+    {
+        get => _product;
+        init
+        {
+            value.IsNotNull(nameof(Product));
+            _product = value;
+        }
+    }
+
+    public Order Order {
+        get => _order;
+        init
+        {
+            value.IsNotNull(nameof(Product));
+            _order = value;
+        }
+    }
+
+    public int Quantity {
+        get => _quantity;
+        init
+        {
+            value.IsNotNull(nameof(Product));
+            _quantity = value;
+        }
+    }
 
 
     // ----------< Calculated Properties >----------
@@ -24,10 +52,6 @@ public record ProductQuantityInOrder
         Order order,
         int quantity)
     {
-        product.IsNotNull(nameof(Product));
-        order.IsNotNull(nameof(Order));
-        quantity.IsPositive(nameof(Quantity));
-
         Product = product;
         Order = order;
         Quantity = quantity;
