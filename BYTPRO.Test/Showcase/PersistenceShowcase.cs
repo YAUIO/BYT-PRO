0eb41d02d89c16d371a9c09dafc50f696ea390df
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-12-03 12:16:16 +0100

    revert

---
+++ BYTPRO.Test/Showcase/PersistenceShowcase.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Text;
+using BYTPRO.Data.Models.Locations;
+using BYTPRO.Data.Models.Locations.Branches;
+using BYTPRO.Data.Models.People;
+using BYTPRO.Data.Models.People.Employees;
+using BYTPRO.Data.Models.People.Employees.Local;
+using BYTPRO.Data.Models.People.Employees.Regional;
+using BYTPRO.Data.Models.Sales;
+using BYTPRO.Data.Models.Sales.Orders;
+using BYTPRO.JsonEntityFramework.Context;
+using BYTPRO.JsonEntityFramework.Extensions;
+using Xunit.Abstractions;
+
+namespace BYTPRO.Test.Showcase;
+
+public class PersistenceShowcase(ITestOutputHelper testOutputHelper)
+{
+    private static string DbRoot => $"{Directory.GetCurrentDirectory()}/Db";
+
+    static PersistenceShowcase()
+    {
+        if (!Directory.Exists(DbRoot)) Directory.CreateDirectory(DbRoot);
+
+        var context = new JsonContextBuilder()
+            // ----------< Locations >----------
+            .AddJsonEntity<PickupPoint>()
+            .WithFileName("pickupPoints")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<Store>()
+            .WithFileName("stores")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<Warehouse>()
+            .WithFileName("warehouses")
+            .BuildEntity()
+
+            // ----------< People >----------
+            .AddJsonEntity<Customer>()
+            .WithFileName("customers")
+            .BuildEntity()
+            // ------------------------------
+            .AddJsonEntity<LocalEmployee>()
+            .WithFileName("localEmployees")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<RegionalEmployee>()
+            .WithFileName("regionalEmployees")
+            .BuildEntity()
+
+            // ----------< Sales >----------
+            .AddJsonEntity<Product>()
+            .WithFileName("products")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<OnlineOrder>()
+            .WithFileName("onlineOrders")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<OfflineOrder>()
+            .WithFileName("offlineOrders")
+            .BuildEntity()
+            //------------------------------
+            .AddJsonEntity<BranchOrder>()
+            .WithFileName("branchOrders")
+            .BuildEntity()
+
+            //------------------------------
+            .WithRoot(new DirectoryInfo(DbRoot))
+            .Build();
+
+        JsonContext.SetContext(context);
+    }
+
+    
+    private void TestClassExtent()
+    {
+        // ----------< Locations >----------
+        var pickupPoint = new PickupPoint(
+            new Address("Street1", "10/2", "app1", "01-234", "City1"),
+            "PickupPoint1",
+            "10:00-22:00",
+            100m,
+            50,
+            10m
+        );
+
+        var store = new Store(
+            new Address("Street2", "20/2", null, "01-345", "City2"),
+            "Store1",
+            "09:00-22:00",
+            1000m,
+            5,
+            500m,
+            3
+        );
+
+        var warehouse = new Warehouse(
+            new Address("Street3", "30/2", null, "01-456", "City3"),
+            "Warehouse1",
+            "00:00-24:00",
+            10000m,
+            50000m,
+            3
+        );
+
+        // ----------< People >----------
+        var customer = new Customer(
+            1,
+            "Artiom",
+            "Bezkorovainyi",
+            "+48000000000",
+            "s30000@pjwstk.edu.pl",
+            "12345678",
+            DateTime.Now
+        );
+
+        var localEmployee = new LocalEmployee(
+            2,
+            "John",
+            "Smith",
+            "+48123456789",
+            "john.smith@gmail.com",
+            "12345",
+            "12345678901",
+            5000m,
+            EmploymentType.FullTime,
+            ["Basics"],
+            "12:00-13:00",
+            store
+        );
+
+        var regionalEmployee = new RegionalEmployee(
+            3,
+            "Jane",
+            "Smith",
+            "+48123456780",
+            "jane.smith@gmail.com",
+            "123456789",
+            "12345678902",
+            10000m,
+            EmploymentType.Intern,
+            "INTERN@12345",
+            SupervisionScope.Technical
+        );
+
+        // ----------< Sales >----------
+        var product1 = new Product(
+            "Product1",
+            "Description1",
+            100m,
+            ["/Product1_1.png", "/Product1_2.png"],
+            10m,
+            new Dimensions(10m, 10m, 10m)
+        );
+        store.AddProductStock(product1, 2);
+        store.AddProductStock(product1, 3);
+
+        var product2 = new Product(
+            "Product2",
+            "Description2",
+            50m,
+            ["/Product2_1.png"],
+            5m,
+            new Dimensions(5m, 5m, 5m)
+        );
+        store.AddProductStock(product2, 10);
+
+
+        // Orders cannot be properly tested now because of circular references.
+
+        /*var onlineOrder = new OnlineOrder(
+            1,
+            DateTime.Now,
+            new Dictionary<Product, int>
+            {
+                { product1, 1 },
+                { product2, 5 }
+            },
+            true,
+            "TRN12345",
+            customer
+        );
+
+        var offlineOrder = new OfflineOrder(
+            2,
+            DateTime.Now,
+            new Dictionary<Product, int>
+            {
+                { product1, 2 },
+                { product2, 2 }
+            },
+            null,
+            store
+        );*/
+
+
+        JsonContext.Context.SaveChanges();
+
+        ShowAll();
+    }
+    
+    private void ShowAll()
+    {
+        var sections = new (string Title, IEnumerable Data)[]
+        {
+            ("Locations", new (string, IEnumerable)[]
+            {
+                ("Branches", Branch.All),
+                ("PickupPoints", PickupPoint.All),
+                ("Stores", Store.All),
+                ("Warehouses", Warehouse.All)
+            }),
+            ("People", new (string, IEnumerable)[]
+            {
+                ("Persons", Person.All),
+                ("Customers", Customer.All),
+                ("Employees", Employee.All),
+                ("LocalEmployees", LocalEmployee.All),
+                ("RegionalEmployees", RegionalEmployee.All)
+            }),
+            ("Sales", new (string, IEnumerable)[]
+            {
+                ("Products", Product.All),
+                ("Orders", Order.All),
+                ("OnlineOrders", OnlineOrder.All),
+                ("OfflineOrders", OfflineOrder.All),
+                ("BranchOrders", BranchOrder.All)
+            })
+        };
+
+        var sb = new StringBuilder();
+
+        foreach (var (title, dataArray) in sections)
+        {
+            sb.AppendLine($"\n\n--------------------> {title} <--------------------");
+
+            foreach (var (name, data) in dataArray.Cast<(string, IEnumerable)>())
+            {
+                sb.AppendLine($"{name}({data.Cast<object>().Count()}): {data.ToJson()}");
+                sb.AppendLine("\n----------------------------------------\n");
+            }
+        }
+
+        testOutputHelper.WriteLine(sb.ToString());
+    }
+
+    [Fact]
+    public void Delete()
+    {
+        if (Directory.Exists(DbRoot)) Directory.Delete(DbRoot, true);
+    }
+}
