f3da3c578849debd3e3346ccf4ec8d836167159d
dredd <1misha.baltovsky@gmail.com>
2025-12-17 01:43:45 +0100

    Refactor employee creation: remove redundant factory methods. Cleanup and update unit tests to match changes.

--- BYTPRO.Test/Data/Inheritance/OverlappingMultiAspectInheritanceTests.cs
+++ BYTPRO.Test/Data/Inheritance/OverlappingMultiAspectInheritanceTests.cs
@@ -1,9 +1,5 @@
-using System.ComponentModel.DataAnnotations;
 using System.Reflection;
-using BYTPRO.Data.Models.Locations;
-using BYTPRO.Data.Models.Locations.Branches;
 using BYTPRO.Data.Models.People.Employees;
-using BYTPRO.Data.Models.People.Employees.Regional;
 using BYTPRO.Test.Data.Factories;
 using ValidationException = BYTPRO.Data.Validation.ValidationException;
 
@@ -11,9 +7,58 @@ namespace BYTPRO.Test.Data.Inheritance;
 
 public class OverlappingMultiAspectInheritanceTests
 {
-    
-    // ----------< RegionalEmployee Inheritance Tests >----------
-    
+    /*
+     People Inheritance tree reminder:
+
+     Person (abstract)
+       │
+       ├─ Customer
+       │
+       └─ Employee (abstract)
+          │
+          ├─ "Roles" (overlapping)
+          │  ├─ Cashier
+          │  ├─ Consultant
+          │  └─ Manager
+          │
+          ├─ LocalEmployee
+          │
+          └─ RegionalEmployee
+    */
+
+    #region ----------< Employee (roles aspect) >----------
+
+    [Fact]
+    public void RoleApiDoesntProvidePublicConstructors()
+    {
+        var emp = PeopleFactory.CreateRegionalEmployee();
+
+        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public));
+        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public));
+        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public));
+    }
+
+    [Fact]
+    public void RoleDeclarationDoesntProvidePublicConstructors()
+    {
+        var emp = PeopleFactory.CreateRegionalEmployee();
+
+        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
+        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
+        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
+    }
+
+    #endregion
+
+    #region ----------< RegionalEmployee >----------
+
+    [Fact]
+    public void CreateRegionalEmployeeWithNoRolesFails()
+    {
+        Assert.Throws<ValidationException>(() =>
+            PeopleFactory.CreateRegionalEmployee(useDefaultRolesWhenNoneProvided: false));
+    }
+
     [Fact]
     public void CreateRegionalEmployeeWithOneRole()
     {
@@ -78,42 +123,25 @@ public class OverlappingMultiAspectInheritanceTests
 
         Assert.Equal(Employee.ManagerialLevel.Senior, employee.ManagerRole.ManagerialLevel);
     }
-    
+
+    #endregion
+
+    #region ----------< LocalEmployee >----------
+
     [Fact]
-    public void CreateRegionalEmployeeWithNoRolesFails()
+    public void CreateLocalEmployeeWithNoRolesFails()
     {
-        Assert.Throws<ValidationException>(() => PeopleFactory.CreateRegionalEmployeeWithNoRolesDefaults());
+        var branch = LocationsFactory.CreatePureBranch();
+
+        Assert.Throws<ValidationException>(() =>
+            PeopleFactory.CreateLocalEmployee(branch, useDefaultRolesWhenNoneProvided: false));
     }
-    
-    [Fact]
-    public void RoleApiDoesntProvidePublicConstructors()
-    {
-        var emp = PeopleFactory.CreateRegionalEmployee();
-        
-        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public));
-        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public));
-        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public));
-    }
-    
-    [Fact]
-    public void RoleDeclarationDoesntProvidePublicConstructors()
-    {
-        var emp = PeopleFactory.CreateRegionalEmployee();
-        
-        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-    }
-        
-    // ----------< LocalEmployee Inheritance Tests >----------
-    
+
     [Fact]
     public void CreateLocalEmployeeWithOneRole()
     {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        
-        var employee = PeopleFactory.CreateLocalEmployee(
-            branch: branch,
+        var branch = LocationsFactory.CreatePureBranch();
+        var employee = PeopleFactory.CreateLocalEmployee(branch,
             cashier: new Employee.CashierParams(999, 123, false)
         );
 
@@ -131,10 +159,8 @@ public class OverlappingMultiAspectInheritanceTests
     [Fact]
     public void CreateLocalEmployeeWithTwoRoles()
     {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        
-        var employee = PeopleFactory.CreateLocalEmployee(
-            branch: branch,
+        var branch = LocationsFactory.CreatePureBranch();
+        var employee = PeopleFactory.CreateLocalEmployee(branch,
             cashier: new Employee.CashierParams(888, 456, true),
             consultant: new Employee.ConsultantParams("Tech", ["English"])
         );
@@ -156,10 +182,8 @@ public class OverlappingMultiAspectInheritanceTests
     [Fact]
     public void CreateLocalEmployeeWithThreeRoles()
     {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        
-        var employee = PeopleFactory.CreateLocalEmployee(
-            branch: branch,
+        var branch = LocationsFactory.CreatePureBranch();
+        var employee = PeopleFactory.CreateLocalEmployee(branch,
             cashier: new Employee.CashierParams(777, 789, false),
             consultant: new Employee.ConsultantParams("PC Tech", ["Polish"]),
             manager: new Employee.ManagerParams(Employee.ManagerialLevel.Senior)
@@ -181,33 +205,5 @@ public class OverlappingMultiAspectInheritanceTests
         Assert.Equal(Employee.ManagerialLevel.Senior, employee.ManagerRole.ManagerialLevel);
     }
 
-    [Fact]
-    public void CreateLocalEmployeeWithNoRolesFails()
-    {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        
-        Assert.Throws<ValidationException>(() => PeopleFactory.CreateLocalEmployeeWithNoRolesDefaults(branch));
-    }
-
-    [Fact]
-    public void LocalRoleApiDoesntProvidePublicConstructors()
-    {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        var emp = PeopleFactory.CreateLocalEmployee(branch: branch);
-    
-        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public));
-        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public));
-        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public));
-    }
-
-    [Fact]
-    public void LocalRoleDeclarationDoesntProvidePublicConstructors()
-    {
-        var branch = LocationsFactory.CreatePureBranch(); 
-        var emp = PeopleFactory.CreateLocalEmployee(branch: branch);
-    
-        Assert.Empty(emp.CashierRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-        Assert.Empty(emp.ManagerRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-        Assert.Empty(emp.ConsultantRole.GetType().GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-    }
+    #endregion
 }
